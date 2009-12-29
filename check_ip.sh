4dfb4be19322347066d5f2e4e2c5e80dded7a441
Randy Morris <randy.morris@rsontech.net>
2009-12-29 12:44:29 -0500

    more verbose log

--- check_ip.sh
+++ check_ip.sh
@@ -4,17 +4,18 @@
 # it has
 #
 # CREATED: 2009-08-10 12:23
-# MODIFIED: 2009-10-21 20:44
+# MODIFIED: 2009-12-29 12:44
 #
  
 IP=$HOME/tmp/ip/current_external_ip
 TEMP=$HOME/tmp/ip/temp_ip
 EMAIL_LIST=$(< $HOME/.email)
-LOG=$HOME/tmp/ip/error_log
+LOG=$HOME/tmp/ip/log
 IP_SERVICE="www.whatismyip.com/automation/n09230945.asp" 
 
 function _save_and_mail {
     cp $TEMP $IP
+    _log "$HOSTNAME is now at $(< $IP); Email sent to $EMAIL_LIST"
     echo "$HOSTNAME is now at $(< $IP)" | mail -s $(< $IP) $EMAIL_LIST
 }
 
