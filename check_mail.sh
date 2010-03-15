ed9d115d7c074a49a687c5720dc12f77d3debaaf
Randy Morris <randy.morris@rsontech.net>
2010-03-15 13:47:10 -0400

    Add section for personal email

--- check_mail.sh
+++ check_mail.sh
@@ -3,9 +3,14 @@
 # check_mail.sh
 #
 # CREATED:  2009-02-02 08:43
-# MODIFIED: 2009-12-01 16:32
+# MODIFIED: 2009-12-29 18:08
 #
 MAILDIR="$HOME/.mail/INBOX/new/"
 mail=$(ls -1 $MAILDIR | wc -l)
 
-[ $mail -gt 0 ] && echo "  $mail mail" || exit 0
+[ $mail -gt 0 ] && echo "  $mail mail"
+
+MAILDIR="$HOME/.personal_mail/INBOX/new/"
+mail=$(ls -1 $MAILDIR | wc -l)
+
+[ $mail -gt 0 ] && echo "  $mail gmail" || exit 0
