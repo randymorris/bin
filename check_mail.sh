c40010298d1d2b2d2f979b4724ca4610b56e7c1c
Randy Morris <randy.morris@rsontech.net>
2010-06-15 17:49:25 -0400

    Change where personal mail is stored

--- check_mail.sh
+++ check_mail.sh
@@ -3,14 +3,14 @@
 # check_mail.sh
 #
 # CREATED:  2009-02-02 08:43
-# MODIFIED: 2009-12-29 18:08
+# MODIFIED: 2010-05-20 09:25
 #
-MAILDIR="$HOME/.mail/INBOX/new/"
+MAILDIR="$HOME/.mail/lists/INBOX/new/"
 mail=$(ls -1 $MAILDIR | wc -l)
 
 [ $mail -gt 0 ] && echo "  $mail mail"
 
-MAILDIR="$HOME/.personal_mail/INBOX/new/"
+MAILDIR="$HOME/.mail/gmail/INBOX/new/"
 mail=$(ls -1 $MAILDIR | wc -l)
 
 [ $mail -gt 0 ] && echo "  $mail gmail" || exit 0
