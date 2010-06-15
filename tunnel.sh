0550f601c48c54273a933714458e0a0c522abd25
Randy Morris <randy.morris@rsontech.net>
2010-06-15 17:50:26 -0400

    Doesn't belong here

--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -i ~randy/.ssh/passwordless_rsa -R 31179:localhost:22 rsontech.net -l randy -n -N
+ssh -g -i ~randy/.ssh/passwordless_rsa -R 31179:localhost:22 rsontech.net -l randy -n -N
