b335f521caad2ca2e68e3a11cc1c81868ea5ad18
Randy Morris <randy.morris@rsontech.net>
2010-11-10 12:30:40 -0500

    Fix typo

--- check_updates.sh
+++ check_updates.sh
@@ -3,9 +3,9 @@
 # check_updates.sh
 #
 # CREATED:  2008-11-03 13:32
-# MODIFIED: 2010-06-02 20:47
+# MODIFIED: 2010-11-10 12:29
 #
-ignore=$(ed -rn '/^\s*IgnorePkg/{s/.*=\s*//; s/\s+/|/gp}' /etc/pacman.conf)
+ignore=$(sed -rn '/^\s*IgnorePkg/{s/.*=\s*//; s/\s+/|/gp}' /etc/pacman.conf)
 updates=$(pacman -Qqu | grep -Ev "^($ignore)$" | wc -l)
 
 [ $updates -gt 0 ] && echo -n "  $updates update"
