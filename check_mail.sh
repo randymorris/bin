a3c9b6841db9f0956e4354ca6d0903aebd4becc9
Randy Morris <randy.morris@rsontech.net>
2010-11-10 12:31:34 -0500

    Update maildirs

--- check_mail.sh
+++ check_mail.sh
@@ -3,14 +3,14 @@
 # check_mail.sh
 #
 # CREATED:  2009-02-02 08:43
-# MODIFIED: 2010-05-20 09:25
+# MODIFIED: 2010-07-22 07:19
 #
-MAILDIR="$HOME/.mail/lists/INBOX/new/"
+MAILDIR="$HOME/.mail/INBOX/new/"
 mail=$(ls -1 $MAILDIR | wc -l)
 
 [ $mail -gt 0 ] && echo "  $mail mail"
 
-MAILDIR="$HOME/.mail/gmail/INBOX/new/"
+MAILDIR="$HOME/.personal_mail/INBOX/new/"
 mail=$(ls -1 $MAILDIR | wc -l)
 
 [ $mail -gt 0 ] && echo "  $mail gmail" || exit 0
